b762131246961d33d96f168ac8f4572503385e1a
Bipin Kumar Ojha <bkojha74@gmail.com>
2025-08-28 00:08:52 +0530

    aded cors

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"go-erp-nlm-mongo/routes"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/joho/godotenv"
 )
 
@@ -14,6 +15,7 @@ func main() {
 	config.ConnectDB()
 
 	app := fiber.New()
+	app.Use(cors.New())
 
 	routes.SetupRoutes(app)
 
